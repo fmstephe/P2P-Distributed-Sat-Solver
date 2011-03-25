a858f689a268f0f3200a78409090c43aaa8a7b3b
Francis Stephens <francisstephens@gmail.com>
2011-03-25 01:13:35 +0000

    Added HopScotchList to the master branch manually.

---
+++ src/org/francis/sat/collections/HopScotchList.java
@@ -0,0 +1,50 @@
+package org.francis.sat.collections;
+
+@SuppressWarnings("unchecked")
+public class HopScotchList<T> {
+
+    private Object[] elems;
+    private int size;
+    
+    public HopScotchList() {
+        this(10);
+    }
+    
+    public HopScotchList(int initialCapacity) {
+        elems = new Object[initialCapacity];
+        size = 0;
+    }
+    
+    public void remove(int i) {
+        assert i < size;
+        assert size != 0;
+        elems[i] = getLast();
+        size--;
+    }
+    
+    public T get(int i) {
+        assert i < size;
+        assert size != 0;
+        return (T)elems[i];
+    }
+    
+    public void add(T elem) {
+        if (elems.length == size) enlarge();
+        elems[size] = elem;
+        size++;
+    }
+    
+    public int size() {
+        return size;
+    }
+    
+    private void enlarge() {
+        Object[] newElems = new Object[size*2];
+        System.arraycopy(elems, 0, newElems, 0, elems.length);
+        elems = newElems;
+    }
+    
+    private T getLast() {
+        return (T) elems[size-1];
+    }
+}
